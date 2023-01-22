2222ceaa861caffb7fd8bf33a6c5665b65f6e6ea
Sanja <gamesg274@gmail.com>
2023-01-22 23:40:56 +0100

    Added more test cases for calculator

--- calculator.js
+++ calculator.js
@@ -1,20 +1,40 @@
 const calculator = {
   add: (num1, num2) => {
-    return num1 + num2;
+    if (testValue(num1, num2)) {
+      return Number(num1) + Number(num2);
+    } 
+    throw new Error("Function expect number, but got wrong value type!");
   },
   subtract: (num1, num2) => {
-    return num1 - num2;
+    if (testValue(num1, num2)) {
+      return Number(num1) - Number(num2);
+    } 
+    throw new Error("Function expect number, but got wrong value type!");
   },
   divide: (num1, num2) => {
-    if (num2 === 0) {
-      throw new Error("Cannot divide by 0");
-    }
+    if (testValue(num1, num2)) {
+      if (num2 === 0) {
+        throw new Error("Cannot divide by 0");
+      }
 
-    return num1 / num2;
+      return Number(num1) / Number(num2);
+    } 
+    throw new Error("Function expect number, but got wrong value type!");
   },
   multiply: (num1, num2) => {
-    return num1 * num2;
+    if (testValue(num1, num2)) {
+      return Number(num1) * Number(num2);
+    } 
+    throw new Error("Function expect number, but got wrong value type!");
   }
+}
+
+function testValue (value1, value2) {
+  if (isNaN(value1) || isNaN(value2)) {
+      return false;
+  }
+  
+  return true;
 }
 
 export default calculator;
--- calculator.test.js
+++ calculator.test.js
@@ -19,3 +19,45 @@ test('divide by 0', () => {
 test('multiply', () => {
   expect(calculator.multiply(3, 3)).toBe(9);
 });
+
+// if wrong format (not number) is passed
+test('wrong format - add', () => {
+  expect(() => calculator.add("test1", "test2")).toThrowError("Function expect number, but got wrong value type!");
+});
+test('wrong format - subtract', () => {
+  expect(() => calculator.subtract("test1", "test2")).toThrowError("Function expect number, but got wrong value type!");
+});
+test('wrong format - divide', () => {
+  expect(() => calculator.divide("test1", "test2")).toThrowError("Function expect number, but got wrong value type!");
+});
+test('wrong format - multiply', () => {
+  expect(() => calculator.multiply("test1", "test2")).toThrowError("Function expect number, but got wrong value type!");
+});
+
+// if wrong format (not number) is passed as one of values
+test('some value is wrong format - add', () => {
+  expect(() => calculator.add("3", "test2")).toThrowError("Function expect number, but got wrong value type!");
+});
+test('some value is wrong format - subtract', () => {
+  expect(() => calculator.subtract("test1", "2")).toThrowError("Function expect number, but got wrong value type!");
+});
+test('some value is wrong format - divide', () => {
+  expect(() => calculator.divide("5", "test2")).toThrowError("Function expect number, but got wrong value type!");
+});
+test('some value is wrong format - multiply', () => {
+  expect(() => calculator.multiply("test1", "2")).toThrowError("Function expect number, but got wrong value type!");
+});
+
+// if string with just numbers are passed
+test('numbers in string format - add', () => {
+  expect(calculator.add("1", "2")).toBe(3);
+});
+test('numbers in string format - subtract', () => {
+  expect(calculator.subtract("3", "2")).toBe(1);
+});
+test('numbers in string format - divide', () => {
+  expect(calculator.divide("9", "3")).toBe(3);
+});
+test('numbers in string format - multiply', () => {
+  expect(calculator.multiply("3", "3")).toBe(9);
+});
